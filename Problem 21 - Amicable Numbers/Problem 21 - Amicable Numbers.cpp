7b2a332e8a99dbe3bc6f420c9b48f607951df8c5
AndrewLangen <andrewlangen@gmail.com>
2024-07-26 18:19:57 -0400

    Solved Problem 21

--- Problem 21 - Amicable Numbers/Problem 21 - Amicable Numbers.cpp
+++ Problem 21 - Amicable Numbers/Problem 21 - Amicable Numbers.cpp
@@ -16,6 +16,8 @@ If $d(a) = b$ and $d(b) = a$, where $a \ne b$, then $a$ and $b$ are an amicable 
 #include <vector>
 #include <stack>
 
+int DivisorSum(int input);
+
 int main()
 {
     //to improve efficiency, previous results will be stored in an array
@@ -25,19 +27,44 @@ int main()
     //but note that that means that all entries are offset by 2 (so array index 0 corresponds to d(2), index 100 corresponds to d(102), etc.)
     int results[9997] = { -1 };
     int offset = 2;
-    std::vector<std::tuple<int, int>> pairs = std::vector<std::tuple<int, int>>();
     for (int i = offset; i < 9999; i++)
     {
-        //checking to make sure we didn't already calculate this one
-        if (results[i] == -1)
+        results[i-offset] = DivisorSum(i);
+    }
+
+    int sum = 0;
+
+    for (int i = offset; i < 9999; i++)
+    {
+        if (i == 220)
+            auto a = 10;
+        int divisors = results[i-offset];
+        if ((i < divisors) //doing it this way means we can add both numbers whenever the first one is hit without worrying about overcounting; also automatically checks the a != b requirement in the problem
+            && (divisors >2)
+            && (divisors <10000)) // prevents array out of bounds errors (array capped at 9997 because we are offsetting
         {
-            //TODO:this
+            if (results[divisors - offset] == i)
+                sum += i + divisors;
         }
     }
 
     std::cout << "result: ";
-    /*std::cout<< */ /*answer goes here << */;
+    std::cout << sum;
     std::cout << std::endl;
     std::getchar();
     
+}
+
+int DivisorSum(int input)
+{
+    int divisor = 2;
+    int sum = 1; // problem gives 1 as a proper divisor, and it will always be in every number, so we can skip checking it and just start from 1
+
+    while (divisor != ((input / 2) + 1))
+    {
+        if (input % divisor == 0)
+            sum += divisor;
+        divisor++;
+    }
+    return sum;  
 }
